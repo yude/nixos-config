e11f9f307f9e97b28c4da38511a18db86463e0a2
yude <i@yude.jp>
2023-01-08 13:19:10 +0900

    Cleanup 2 & rm hardware-configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -24,10 +24,6 @@ in
 
   networking.resolvconf.dnsExtensionMechanism = false;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
 
